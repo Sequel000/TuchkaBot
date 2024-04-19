b0aac0d4cb75eb0a8d376c9178f3a67ab5570946
Sequel000 <sequelQ000@gmail.com>
2024-04-19 20:16:36 +0300

    Created file with class for text translation

--- Translate/translate.py
+++ Translate/translate.py
@@ -1,14 +1,14 @@
 from googletrans import Translator
 
-
+# Класс для перевода текста
 class Translate:
-
+    # Создаем переменную содежащую класс библиотеки google translate
     def __init__(self):
         self.translator = Translator()
-
+    # Функция перевода с "русского" на "английский"
     def ru_en(self, text):
         return self.translator.translate(text, dest='en').text
-
+    # Функция перевода с "английского" на "русский"
     def en_ru(self, text):
         return self.translator.translate(text, dest='ru').text
 
